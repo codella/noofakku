33c1b74164c267d632013abad53cc2aff22ef5c3
maco <mauro.codella@gmail.com>
2013-12-10 13:52:31 +0100

    Add open bracket instruction

---
+++ lib/noofakku/instruction/open_bracket.rb
@@ -0,0 +1,21 @@
+class OpenBracket
+  def hook
+    '['
+  end
+
+  def perform(processor, memory, program)
+    raise "processor cannot be nil" unless processor != nil
+    raise "memory cannot be nil" unless memory != nil
+    raise "program cannot be nil" unless program != nil
+    raise "ïnstruction pointer must point to a '#{hook}'" unless program[processor.instruction_pointer] == hook
+    if memory[processor.data_pointer] != 0 then return end
+    unclosed_brackets = 1
+    pointer_to_current_instruction = processor.instruction_pointer
+    until unclosed_brackets == 0
+    	processor.instruction_pointer += 1
+    	if processor.instruction_pointer == program.length then raise "no matching ']' for '[' at #{pointer_to_current_instruction}" end
+    	if program[processor.instruction_pointer] == hook then unclosed_brackets += 1 end
+    	if program[processor.instruction_pointer] == "]" then unclosed_brackets -= 1 end
+    end
+  end
+end
---
+++ test/instruction/open_bracket_test.rb
@@ -0,0 +1,53 @@
+require 'test/unit'
+require 'noofakku/instruction/open_bracket'
+require 'shoulda'
+require 'ostruct'
+
+class OpenBracketTest < Test::Unit::TestCase
+
+  context "Hook" do
+
+    should "return '[' as hook character" do
+      assert_equal "[", OpenBracket.new.hook
+    end
+
+  end
+
+  context "Execution" do
+
+    setup do
+      @instance = OpenBracket.new
+    end
+
+    should "throw on nil processor" do
+      assert_raise (RuntimeError) { @instance.perform(nil, Object.new, Object.new) }
+    end
+
+    should "throw on nil memory" do
+      assert_raise (RuntimeError) { @instance.perform(Object.new, nil, Object.new) }
+    end
+
+    should "throw on nil program" do
+      assert_raise (RuntimeError) { @instance.perform(Object.new, Object.new, nil) }
+    end
+
+    should "throw if instruction pointer is not pointing to an open bracket" do
+      spy_processor = OpenStruct.new(instruction_pointer: 0)
+      assert_raise (RuntimeError) { @instance.perform(spy_processor, Object.new, "_") }
+    end
+
+    should "leave the instruction pointer as is if data pointer points to a cell which value is not zero" do
+      spy_processor = OpenStruct.new(instruction_pointer: 0, data_pointer: 0)
+      @instance.perform(spy_processor, [42], "[_]_")
+      assert_equal 0, spy_processor.instruction_pointer
+    end
+
+    should "change the instruction pointer in order to point to the matching ']' instruction" do
+      spy_processor = OpenStruct.new(instruction_pointer: 0, data_pointer: 0)
+      @instance.perform(spy_processor, [0], "[_]_")
+      assert_equal 2, spy_processor.instruction_pointer
+    end
+
+  end
+
+end
