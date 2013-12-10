0cf132e70b8873c62d15db54ca4244c50cc1bedc
Mauro Codella <mauro.codella@gmail.com>
2013-12-10 19:49:50 +0100

    Add closed bracket instruction

---
+++ lib/noofakku/instruction/closed_bracket.rb
@@ -0,0 +1,22 @@
+class ClosedBracket
+  def hook
+    ']'
+  end
+
+  def perform(processor, memory, program)
+    raise "processor cannot be nil" unless processor != nil
+    raise "memory cannot be nil" unless memory != nil
+    raise "program cannot be nil" unless program != nil
+    raise "instruction pointer must point to a '#{hook}'" unless program[processor.instruction_pointer] == hook
+    if memory[processor.data_pointer] == 0 then return end
+    unmatched_brackets = 1
+    pointer_to_current_instruction = processor.instruction_pointer
+    until unmatched_brackets == 0
+    	processor.instruction_pointer -= 1
+    	if processor.instruction_pointer == -1 then raise "no matching '[' for ']' at #{pointer_to_current_instruction}"
+    	elsif program[processor.instruction_pointer] == hook then unmatched_brackets += 1
+    	elsif program[processor.instruction_pointer] == "[" then unmatched_brackets -= 1
+      end
+    end
+  end
+end
