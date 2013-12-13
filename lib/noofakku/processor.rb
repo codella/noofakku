304514df290b84e551118e44a64b968fabeeea6b
maco <mauro.codella@gmail.com>
2013-12-13 15:30:17 +0100

    Associate noop instruction to any other symbol that is not explicitly recognized by the VM

--- lib/noofakku/processor.rb
+++ lib/noofakku/processor.rb
@@ -1,19 +1,12 @@
-require 'noofakku/instruction/open_bracket'
-require 'noofakku/instruction/closed_bracket'
-require 'noofakku/instruction/inc'
-require 'noofakku/instruction/dec'
-require 'noofakku/instruction/prev'
-require 'noofakku/instruction/next'
-require 'noofakku/instruction/input'
-require 'noofakku/instruction/output'
-
 module Noofakku
 
   class Processor
+
+    attr_accessor :instruction_pointer, :data_pointer
   	
-  	def initialize(instructions)
-      raise "instructions cannot be nil" unless instructions != nil
-      @instructions = instructions
+  	def initialize(instruction_to_handler)
+      raise "instruction_to_handler cannot be nil" unless instruction_to_handler != nil
+      @instruction_to_handler = instruction_to_handler
       @instruction_pointer = @data_pointer = 0
   	end
 
@@ -23,7 +16,7 @@ module Noofakku
       raise "input cannot be nil" unless input != nil
       raise "output cannot be nil" unless output != nil
       while (0...program.length).include? @instruction_pointer
-        instruction = @instructions[program[@instruction_pointer]]
+        instruction = @instruction_to_handler[program[@instruction_pointer]]
         instruction.perform(self, memory, program, input, output)
         @instruction_pointer += 1
       end 
