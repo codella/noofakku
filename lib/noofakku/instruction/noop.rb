86e711a547f0428d61b0e097e2d0a2d4a2e08215
maco <mauro.codella@gmail.com>
2013-12-13 15:23:46 +0100

    Add noop instruction for unrecognized program symbols

---
+++ lib/noofakku/instruction/noop.rb
@@ -0,0 +1,11 @@
+module Noofakku
+
+  class Noop
+
+    def perform(processor, memory, program, input, output)
+    	# lazyness is prevailing over me... :(
+    end
+
+  end
+
+end
---
+++ test/instruction/noop_test.rb
@@ -0,0 +1,66 @@
+require 'test/unit'
+require 'noofakku/instruction/noop'
+require 'shoulda'
+require 'ostruct'
+
+module Noofakku
+
+  class NoopTest < Test::Unit::TestCase
+
+    context "Execution" do
+
+      setup do
+        @instance = Noop.new
+      end
+
+      should "leave alone the data pointer" do
+        spy_processor = OpenStruct.new(data_pointer: 0)
+        @instance.perform(spy_processor, [], [], ->{0}, ->value{})
+        assert_equal 0, spy_processor.data_pointer
+      end
+
+      should "leave alone the instruction pointer" do
+        spy_processor = OpenStruct.new(instruction_pointer: 0)
+        @instance.perform(spy_processor, [], [], ->{0}, ->value{})
+        assert_equal 0, spy_processor.instruction_pointer
+      end
+
+      should "leave alone the memory" do
+        touched = false
+        not_to_be_touched = [].freeze
+        begin
+          @instance.perform(Object.new, not_to_be_touched, [], ->{0}, ->value{})
+        rescue
+          touched = true
+        end
+        assert_false touched
+      end
+
+      should "leave alone the program" do
+        touched = false
+        not_to_be_touched = [].freeze
+        begin
+          @instance.perform(Object.new, [], not_to_be_touched, ->{0}, ->value{})
+        rescue
+          touched = true
+        end
+        assert_false touched
+      end
+
+      should "leave alone the input lambda" do
+        invoked = false
+        @instance.perform(Object.new, [], [], ->{ invoked = true }, ->value{})
+        assert_false invoked
+      end
+
+      should "leave alone the output lambda" do
+        invoked = false
+        @instance.perform(Object.new, [], [], ->{}, ->value{ invoked = true })
+        assert_false invoked
+      end
+
+    end
+
+  end
+
+end
