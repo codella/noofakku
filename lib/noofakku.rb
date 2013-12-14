de828f46d02f643f48886efebdfb02a5fd9f4da1
Mauro Codella <mauro.codella@gmail.com>
2013-12-14 10:28:54 +0100

    Use single quoted strings wherever possible

--- lib/noofakku.rb
+++ lib/noofakku.rb
@@ -12,14 +12,14 @@ module Noofakku
   class VM
     def self.start(program, input, output)
     	processor = Processor.new(Hash.new(Noop.new).merge!({
-      	">" => Next.new,
-      	"<" => Prev.new,
-      	"+" => Inc.new,
-      	"-" => Dec.new,
-      	"." => Output.new,
-      	"," => Input.new,
-      	"[" => OpenBracket.new("[", "]"),
-      	"]" => ClosedBracket.new("[", "]")
+      	'>' => Next.new,
+      	'<' => Prev.new,
+      	'+' => Inc.new,
+      	'-' => Dec.new,
+      	'.' => Output.new,
+      	',' => Input.new,
+      	'[' => OpenBracket.new('[', ']'),
+      	']' => ClosedBracket.new('[', ']')
       }))
       processor.run(program.clone.freeze, Array.new(30_000, 0), input, output)
     end
