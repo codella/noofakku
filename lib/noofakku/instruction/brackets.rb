2d5ba4b2bdccd7179ba0c58276e52a64ab4c57a8
Mauro Codella <mauro.codella@gmail.com>
2013-12-15 17:59:05 +0100

    fix

--- lib/noofakku/instruction/brackets.rb
+++ lib/noofakku/instruction/brackets.rb
@@ -1,6 +1,6 @@
 module Noofakku
 
-  class ClosedBracket
+  class Brackets
 
     def initialize(open, closed)
       @open, @closed = open, closed
@@ -10,14 +10,14 @@ module Noofakku
       raise "processor cannot be nil" unless processor != nil
       raise "memory cannot be nil" unless memory != nil
       raise "program cannot be nil" unless program != nil
-      if memory[processor.data_pointer] == 0 then return end
+      if memory[processor.data_pointer] != 0 then return end
       unmatched_brackets = 1
       pointer_to_current_instruction = processor.instruction_pointer
       until unmatched_brackets == 0
-      	processor.instruction_pointer -= 1
-      	if processor.instruction_pointer == -1 then raise "no matching '#{@open}' for '#{@closed}' at #{pointer_to_current_instruction}"
-      	elsif program[processor.instruction_pointer] == @closed then unmatched_brackets += 1
-      	elsif program[processor.instruction_pointer] == @open then unmatched_brackets -= 1
+      	processor.instruction_pointer += 1
+      	if processor.instruction_pointer == program.length then raise "no matching '#{@closed}' for '#{@open}' at #{pointer_to_current_instruction}"
+      	elsif program[processor.instruction_pointer] == @open then unmatched_brackets += 1
+      	elsif program[processor.instruction_pointer] == @closed then unmatched_brackets -= 1
         end
       end
     end
