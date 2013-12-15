8eb921f892fc418d267a036d9b44fa089ba72b63
Mauro Codella <mauro.codella@gmail.com>
2013-12-15 22:37:48 +0100

    Apply refactor on brackets

--- lib/noofakku/instruction/brackets.rb
+++ lib/noofakku/instruction/brackets.rb
@@ -10,14 +10,20 @@ module Noofakku
       raise "processor cannot be nil" unless processor != nil
       raise "memory cannot be nil" unless memory != nil
       raise "program cannot be nil" unless program != nil
-      if memory[processor.data_pointer] != 0 then return end
+      value_at_data_pointer = memory[processor.data_pointer]
+      this_bracket = program[processor.instruction_pointer]
+      other_bracket = this_bracket != @open ? @open :  @closed
+      if (value_at_data_pointer != 0 and this_bracket == @open) or (value_at_data_pointer == 0 and this_bracket == @closed)
+        return
+      end
       unmatched_brackets = 1
       pointer_to_current_instruction = processor.instruction_pointer
+      direction = this_bracket == @open ? 1 : -1
       until unmatched_brackets == 0
-      	processor.instruction_pointer += 1
-      	if processor.instruction_pointer == program.length then raise "no matching '#{@closed}' for '#{@open}' at #{pointer_to_current_instruction}"
-      	elsif program[processor.instruction_pointer] == @open then unmatched_brackets += 1
-      	elsif program[processor.instruction_pointer] == @closed then unmatched_brackets -= 1
+      	processor.instruction_pointer += direction
+      	if not (0...program.length).include? processor.instruction_pointer then raise "no matching bracket for that at #{pointer_to_current_instruction}"
+      	elsif program[processor.instruction_pointer] == this_bracket then unmatched_brackets += 1
+      	elsif program[processor.instruction_pointer] == other_bracket then unmatched_brackets -= 1
         end
       end
     end
